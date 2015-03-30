7e2791c9dd4cdd65bc30f227b6a85a4ef1e53aec
cyberfunk <hello@cyberfunk.org>
2015-03-30 12:09:51 +0800

    Update hook fixture.

--- test/fixture/hook-plugin.js
+++ test/fixture/hook-plugin.js
@@ -1,24 +1,25 @@
+var args;
+
+// hook function, invoked on construction
+// best to declare outside the plugin function
+// so there is only a single reference
+function hook() {
+  // constructor arguments
+  args = Array.prototype.slice.call(arguments);
+
+  this.hooked = function hooked(cb) {
+    cb();
+  }
+
+  this.getConstructorArgs = function getConstructorArgs() {
+    return args;
+  }
+}
+
 /**
  *  Plugin method invoked in the scope of the class prototype.
  */
 module.exports = function plugin() {
-
-  var args;
-
-  // hook function, invoked on construction
-  function hook() {
-    // constructor arguments
-    args = Array.prototype.slice.call(arguments);
-
-    this.hooked = function hooked(cb) {
-      cb();
-    }
-
-    this.getConstructorArgs = function getConstructorArgs() {
-      return args;
-    }
-  }
-
   // register the constructor hook
   this.register(hook);
 }
